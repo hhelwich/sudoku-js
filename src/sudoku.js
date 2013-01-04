e392c6fd4418df807c6cbc3640c7f50f1f29a3b0
Hendrik Helwich <hendrik@helwich.de>
2013-01-04 01:52:13 +0100

    implement calculation of row/column/block candidate sets

--- src/sudoku.js
+++ src/sudoku.js
@@ -10,32 +10,164 @@
     "use strict";
 
     var
-        addEase = function (rows, cols, fieldArray) {
+        createField = function (rows, cols, fieldArray) {
             var i;
             if (fieldArray === undefined) { // no buffer given => create new array initialized with null
                 fieldArray = [];
-                fieldArray.length = rows * cols;
-                fieldArray.rows = rows;
-                fieldArray.cols = cols;
-                for (i = fieldArray.length - 1; i >= 0; i-= 1) {
+                fieldArray.length = rows * cols * rows * cols; // number of elements
+                fieldArray.size = rows * cols; // height and width
+                fieldArray.rows = rows; // number of elements in a column of a block
+                fieldArray.cols = cols; // number of elements in a row of a block
+                for (i = fieldArray.length - 1; i >= 0; i -= 1) {
                     fieldArray[i] = null;
                 }
             }
 
             // add convenient getter
             fieldArray.get = function (row, col) {
-                return this[col + row * this.cols];
+                return this[col + row * this.size];
             };
 
             // add convenient setter
             fieldArray.set = function (row, col, value) {
-                this[col + row * this.cols] = value;
+                if (value < 0 || value >= this.size) {
+                    throw "invalid value " + value;
+                }
+                this[col + row * this.size] = value;
+            };
+
+            fieldArray.solve = function () {
+                var ctField = candidateTrackField(this);
             };
 
             return fieldArray;
-        };
+        },
+        candidateTrackField = (function () {
+            // only needed for testing
+            var getCandidates = function (bitset, size) {
+                var ret = [],
+                    i;
+                for (i = 0; i < size; i += 1) {
+                    if ((bitset & (1 << i)) !== 0) {
+                        ret.push(i);
+                    }
+                }
+                return ret;
+            };
+
+            return function (field) {
+                // bitsets with set ones for all possible symbols (default sudoku: 9)
+                // numbers: 64 bit
+                var rows,
+                    cols,
+                    blocks,
+                    r,
+                    c,
+                    b,
+                    e,
+                    maxBit,
+                    rebuildBitsetsFor = function (row, col) {
+                        var block, i, j, e, rd, cd;
+                        block = (~~(row / field.rows)) * field.rows + ~~(col / field.cols);
+                        // add all possible elements to the three bitsets
+                        rows[row] = maxBit;
+                        cols[col] = maxBit;
+                        blocks[block] = maxBit;
+                        // reduce row and column bitsets
+                        for (j = 0; j < field.size; j += 1) {
+                            e = field.get(row, j);
+                            if (e !== null) {
+                                rows[row] &= ~(1 << e);
+                            }
+                            e = field.get(j, col);
+                            if (e !== null) {
+                                cols[col] &= ~(1 << e);
+                            }
+                        }
+                        // reduce block bitset
+
+                        rd = (~~(row / field.rows)) * field.rows;
+                        cd = (~~(col / field.cols)) * field.cols;
+                        for (i = 0; i < field.rows; i += 1) {
+                            for (j = 0; j < field.cols; j += 1) {
+                                e = field.get(i + rd, j + cd);
+                                if (e !== null) {
+                                    blocks[block] &= ~(1 << e);
+                                }
+                            }
+                        }
+                    };
+                if (field.size > 63) {
+                    throw "field is to big for this implementation";
+                }
+                maxBit = (1 << field.size) - 1;
+                // initialize arrays with full bitsets
+                rows = [];
+                rows.length = field.size;
+                for (r = field.size - 1; r >= 0; r -= 1) {
+                    rows[r] = maxBit;
+                }
+                cols = rows.slice();
+                blocks = rows.slice();
+
+                // reduce bitsets by pre-filled field values
+                for (r = field.size - 1; r >= 0; r -= 1) {
+                    for (c = field.size - 1; c >= 0; c -= 1) {
+                        e = field.get(r, c);
+                        if (e !== null) {
+                            b = (~~(r / field.rows)) * field.rows + ~~(c / field.cols); // block index to element
+                            e = ~(1 << e);
+                            rows[r] &= e;
+                            cols[c] &= e;
+                            blocks[b] &= e;
+                        }
+                    }
+                }
+                return {
+                    get: function (row, col) {
+                        return field.get(row, col);
+                    },
+                    set: function (row, col, value) {
+                        var current = this.get(row, col),
+                            block;
+                        if (value === null) {
+                            if (current !== null) {
+                                field.set(row, col, null);
+                                rebuildBitsetsFor(row, col);
+                            }
+                        } else {
+                            field.set(row, col, value);
+                            if (current === null) {
+                                // remove value from corresponding bitsets
+                                block = (~~(row / field.rows)) * field.rows + ~~(col / field.cols);
+                                value = ~(1 << value);
+                                rows[row] &= value;
+                                cols[col] &= value;
+                                blocks[block] &= value;
+                            } else {
+                                rebuildBitsetsFor(row, col);
+                            }
+                        }
+                    },
+                    // method only needed for testing
+                    getRowCandidates: function (row) {
+                        return getCandidates(rows[row], field.size);
+                    },
+                    // method only needed for testing
+                    getColCandidates: function (col) {
+                        return getCandidates(cols[col], field.size);
+                    },
+                    // method only needed for testing
+                    getBlockCandidates: function (block) {
+                        return getCandidates(blocks[block], field.size);
+                    }
+
+                };
+            };
+        }());
 
     global.sudoku = {
-        addEase : addEase
+        createField: createField,
+        candidateTrackField: candidateTrackField
     };
 }(window));
--- test/sudoku_test.js
+++ test/sudoku_test.js
@@ -21,14 +21,124 @@
  raises(block, [expected], [message])
  */
 
-test( "test addEase length", function() {
-    strictEqual(sudoku.addEase(3, 4).length, 3 * 4);
+test( "test createField length", function() {
+    strictEqual(sudoku.createField(3, 4).length, 3 * 4 * 3 * 4);
 });
 
-test( "test addEase get", function() {
-    var field = sudoku.addEase(2, 2);
-    strictEqual(field.get(0, 0), null);
-    strictEqual(field.get(1, 1), null);
-    strictEqual(field.get(1, 2), undefined);
-    strictEqual(field.get(2, 1), undefined);
+test( "test createField size", function() {
+    strictEqual(sudoku.createField(3, 4).size, 3 * 4);
 });
+
+test( "test createField get", function() {
+    var field = sudoku.createField(2, 3);
+    strictEqual(field.get(0, 0), null);
+    strictEqual(field.get(5, 5), null);
+    strictEqual(field.get(5, 6), undefined);
+    strictEqual(field.get(6, 5), undefined);
+});
+
+test( "test createField set / get", function() {
+    var field = sudoku.createField(2, 3);
+    strictEqual(field.get(5, 5), null);
+    field.set(2, 3, 3);
+    strictEqual(field.get(2, 3), 3);
+});
+
+test( "test createField array index", function() {
+    var field = sudoku.createField(2, 3);
+    strictEqual(field.get(1, 2), null);
+    field.set(0, 1, 2);
+    field.set(1, 0, 3);
+    field.set(1, 2, 4);
+    strictEqual(field.get(0, 1), 2);
+    strictEqual(field.get(1, 0), 3);
+    strictEqual(field.get(1, 2), 4);
+    strictEqual(field[0 * field.size + 1], 2);
+    strictEqual(field[1 * field.size + 0], 3);
+    strictEqual(field[1 * field.size + 2], 4);
+});
+
+test( "test candidateTrackField", function() {
+    var field = sudoku.createField(2, 3),
+        ctField;
+    field.set(0, 0, 0);
+    field.set(1, 1, 1);
+    field.set(2, 2, 2);
+    field.set(1, 3, 3);
+    field.set(0, 4, 4);
+    field.set(1, 5, 5);
+    field.set(2, 4, 0);
+    field.set(3, 4, 1);
+    field.set(4, 4, 2);
+    field.set(5, 3, 4);
+    ctField = sudoku.candidateTrackField(field);
+    deepEqual(ctField.getRowCandidates(0), [1,2,3,5]);
+    deepEqual(ctField.getRowCandidates(1), [0,2,4]);
+    deepEqual(ctField.getRowCandidates(2), [1,3,4,5]);
+    deepEqual(ctField.getRowCandidates(3), [0,2,3,4,5]);
+    deepEqual(ctField.getRowCandidates(4), [0,1,3,4,5]);
+    deepEqual(ctField.getRowCandidates(5), [0,1,2,3,5]);
+    deepEqual(ctField.getColCandidates(0), [1,2,3,4,5]);
+    deepEqual(ctField.getColCandidates(1), [0,2,3,4,5]);
+    deepEqual(ctField.getColCandidates(2), [0,1,3,4,5]);
+    deepEqual(ctField.getColCandidates(3), [0,1,2,5]);
+    deepEqual(ctField.getColCandidates(4), [3,5]);
+    deepEqual(ctField.getColCandidates(5), [0,1,2,3,4]);
+    deepEqual(ctField.getBlockCandidates(0), [2,3,4,5]);
+    deepEqual(ctField.getBlockCandidates(1), [0,1,2]);
+    deepEqual(ctField.getBlockCandidates(2), [0,1,3,4,5]);
+    deepEqual(ctField.getBlockCandidates(3), [2,3,4,5]);
+    deepEqual(ctField.getBlockCandidates(4), [0,1,2,3,4,5]);
+    deepEqual(ctField.getBlockCandidates(5), [0,1,3,5]);
+    // remove 5 from three sets
+    ctField.set(3, 1, 5);
+    deepEqual(ctField.getRowCandidates(3), [0,2,3,4]);
+    deepEqual(ctField.getColCandidates(1), [0,2,3,4]);
+    deepEqual(ctField.getBlockCandidates(2), [0,1,3,4]);
+    // replace with 3
+    ctField.set(3, 1, 3);
+    deepEqual(ctField.getRowCandidates(3), [0,2,4,5]);
+    deepEqual(ctField.getColCandidates(1), [0,2,4,5]);
+    deepEqual(ctField.getBlockCandidates(2), [0,1,4,5]);
+    // remove element
+    ctField.set(3, 1, null);
+    deepEqual(ctField.getRowCandidates(3), [0,2,3,4,5]);
+    deepEqual(ctField.getColCandidates(1), [0,2,3,4,5]);
+    deepEqual(ctField.getBlockCandidates(2), [0,1,3,4,5]);
+});
+/* ignore test
+test( "test solve", function() {
+    var field = sudoku.createField(3, 3),
+        expected = [
+            5, 8, 2, 6, 7, 3, 4, 0, 1,
+            3, 7, 6, 4, 0, 1, 8, 2, 5,
+            0, 1, 4, 8, 5, 2, 7, 6, 3,
+            8, 2, 1, 5, 4, 0, 3, 7, 6,
+            4, 5, 7, 1, 3, 6, 2, 8, 0,
+            6, 3, 0, 2, 8, 7, 5, 1, 4,
+            2, 0, 8, 3, 6, 4, 1, 5, 7,
+            7, 4, 5, 0, 1, 8, 6, 3, 2,
+            1, 6, 3, 7, 2, 5, 0, 4, 8];
+    // minimal unique solvable field with 17 entries
+    field.set(0, 7, 0);
+    field.set(1, 0, 3);
+    field.set(2, 1, 1);
+    field.set(3, 4, 4);
+    field.set(3, 6, 3);
+    field.set(3, 8, 6);
+    field.set(4, 2, 7);
+    field.set(4, 6, 2);
+    field.set(5, 2, 0);
+    field.set(5, 4, 8);
+    field.set(6, 0, 2);
+    field.set(6, 3, 3);
+    field.set(6, 6, 1);
+    field.set(7, 1, 4);
+    field.set(7, 3, 0);
+    field.set(8, 3, 7);
+    field.set(8, 5, 5);
+    field.solve();
+    ok(solve(field));
+    strictEqual(field, expected, "validate solution");
+});
+*/
