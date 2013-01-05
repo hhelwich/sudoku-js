3559217bbf23a0c422619debbfac9bbbb1d268b7
Hendrik Helwich <hendrik@helwich.de>
2013-01-06 00:06:42 +0100

    add control for different sizes also validate on complete

--- js/main.js
+++ js/main.js
@@ -17,31 +17,74 @@
                     $(window).scrollLeft()) + "px"
             });
         },
-        symb = "123456789";
+        symb = "123456789ABCDEF",
+        validateInputs = function (inputs) {
+            var i, idx, val;
+            for (i = 0; i < inputs.length; i += 1) {
+                if (inputs[i].val().length === 0) { // field is not complete
+                    return;
+                }
+            }
+            // field is complete => validate content
+            for (i = 0; i < inputs.length; i += 1) {
+                idx = inputs[i].attr('id');
+                val = inputs[i].val();
+                if (field.solution[idx] !== symb.indexOf(val)) {
+                    inputs[i].css("color", "#FF0000");
+                } else {
+                    inputs[i].css("color", "#00FF00");
+                }
+            }
+        },
+        createField = function () {
+            var input,
+                inputs = [],
+                handleKeyUp = function () {
+                    var input = $(this);
+                    input.val($.trim(input.val()));
+                    validateInputs(inputs);
+                },
+                size = $("select option:selected").val().split("x");
 
-    // create empty sudoku 9x9 field table
-    table = $("<table/>");
-    for (i = 0; i < 9; i += 1) {
-        row = $("<tr/>");
-        for (j = 0; j < 9; j += 1) {
-            row.append('<td class="block' + (~~((i % 6) / 3) + ~~((j % 6) / 3)) % 2 + '"/>');
-        }
-        table.append(row);
-    }
-    container.append(table);
-    table = $("td");
+            // create empty sudoku field
+            field = sudoku.createField(size[0], size[1]);
+
+            // create empty sudoku 9x9 field table
+            table = $("<table/>");
+            for (i = 0; i < field.size; i += 1) {
+                row = $("<tr/>");
+                for (j = 0; j < field.size; j += 1) {
+                    row.append('<td class="block' +
+                        Math.floor(((i % (field.rows * 2)) / field.rows) +
+                            Math.floor((j % (field.cols * 2)) / field.cols)) % 2 + '"/>');
+                }
+                table.append(row);
+            }
+            container.empty().append(table);
+            table = $("td");
+            // generate sudoku
+            field.generate();
+            center();
+
+            // fill up table
+            for (i = 0; i < field.length; i += 1) {
+                if (field[i] === null) {
+                    input = $('<input id="' + i + '" type="text"/>');
+                    inputs.push(input);
+                    input.keyup(handleKeyUp);
+                    $(table.get(i)).append(input);
+                } else {
+                    $(table.get(i)).text(symb.charAt(field[i]));
+                }
+            }
+
+        };
 
     // center content on page
     $(window).resize(center);
-    center();
 
-    // generate sudoku
-    field = sudoku.createField(3, 3);
-    field.generate();
+    $("input[type=button]").click(createField);
 
-    // fill up table
-    for (i = 0; i < 81; i += 1) {
-        $(table.get(i)).html(field[i] === null ? '<input type="text"/>' : symb.charAt(field[i]));
-    }
+    createField();
 
 }(jQuery));
