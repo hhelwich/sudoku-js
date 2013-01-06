949183834050fb30647652c59b952d93fd4e9655
Hendrik Helwich <hendrik@helwich.de>
2013-01-06 18:56:07 +0100

    change test order

--- test/sudoku_test.js
+++ test/sudoku_test.js
@@ -21,6 +21,8 @@
  raises(block, [expected], [message])
  */
 
+// test public api
+
 test( "test createField length", function() {
     strictEqual(hhelwi.sudoku.create(3, 4).length, 3 * 4 * 3 * 4);
 });
@@ -85,6 +87,119 @@ function createCandidateTrackField() {
     ctField = hhelwi.sudoku.internals.candidateTrackField(field);
     return ctField;
 }
+
+test( "test solve", function() {
+    var field = hhelwi.sudoku.create(3, 3),
+        expected = [
+            5, 8, 2, 6, 7, 3, 4, 0, 1,
+            3, 7, 6, 4, 0, 1, 8, 2, 5,
+            0, 1, 4, 8, 5, 2, 7, 6, 3,
+            8, 2, 1, 5, 4, 0, 3, 7, 6,
+            4, 5, 7, 1, 3, 6, 2, 8, 0,
+            6, 3, 0, 2, 8, 7, 5, 1, 4,
+            2, 0, 8, 3, 6, 4, 1, 5, 7,
+            7, 4, 5, 0, 1, 8, 6, 3, 2,
+            1, 6, 3, 7, 2, 5, 0, 4, 8];
+    // minimal unique solvable field with 17 entries
+    field.set(0, 7, 0);
+    field.set(1, 0, 3);
+    field.set(2, 1, 1);
+    field.set(3, 4, 4);
+    field.set(3, 6, 3);
+    field.set(3, 8, 6);
+    field.set(4, 2, 7);
+    field.set(4, 6, 2);
+    field.set(5, 2, 0);
+    field.set(5, 4, 8);
+    field.set(6, 0, 2);
+    field.set(6, 3, 3);
+    field.set(6, 6, 1);
+    field.set(7, 1, 4);
+    field.set(7, 3, 0);
+    field.set(8, 3, 7);
+    field.set(8, 5, 5);
+    ok(field.solve());
+    deepEqual(field, expected, "validate solution");
+});
+
+test( "test solve empty", function() {
+    ok(hhelwi.sudoku.create(3, 3).solve(), "solve empty 9x9");
+});
+
+test( "test generate from empty", function() {
+    var field = hhelwi.sudoku.create(3, 3), f2;
+    ok(field.generate(), "generate from empty 9x9");
+    f2 = field.clone();
+    notDeepEqual(f2, field.solution);
+    f2.solve();
+    deepEqual(f2, field.solution);
+});
+
+// ------------------------------------------------------------- test internals
+
+test( "test internal util firstSetBit", function() {
+    var firstSetBit = hhelwi.sudoku.internals.firstSetBit;
+    strictEqual(firstSetBit(0), -1);
+    strictEqual(firstSetBit(1), 0);
+    strictEqual(firstSetBit(2), 1);
+    strictEqual(firstSetBit(3), 0);
+    strictEqual(firstSetBit(4), 2);
+    strictEqual(firstSetBit(~0), 0);
+    strictEqual(firstSetBit(1 << 31), 31);
+    // works only up to 32 bits as double64 gets converted to int32 for bit operations
+    strictEqual(firstSetBit(1 << 32), 0);
+});
+
+test( "test internal util numberOfSetBits", function() {
+    var numberOfSetBits = hhelwi.sudoku.internals.numberOfSetBits;
+    strictEqual(numberOfSetBits(0), 0);
+    strictEqual(numberOfSetBits(1), 1);
+    strictEqual(numberOfSetBits(2), 1);
+    strictEqual(numberOfSetBits(4), 1);
+    strictEqual(numberOfSetBits(8), 1);
+    strictEqual(numberOfSetBits(16), 1);
+    strictEqual(numberOfSetBits(3), 2);
+    strictEqual(numberOfSetBits(6), 2);
+    strictEqual(numberOfSetBits(15), 4);
+    strictEqual(numberOfSetBits(31), 5);
+    strictEqual(numberOfSetBits(7 << 29), 3);
+    strictEqual(numberOfSetBits(7 << 30), 2);
+    strictEqual(numberOfSetBits(7 << 31), 1);
+    strictEqual(numberOfSetBits(7 << 32), 3); // why?
+    strictEqual(numberOfSetBits(~0), 32);
+    strictEqual(numberOfSetBits(~0 << 1), 31);
+});
+
+test( "test shuffle", function() {
+    var shuffle = hhelwi.sudoku.internals.shuffle;
+    var array = [0,1,2,3,4,5,6,7,8],
+        ar2,
+        i,
+        j,
+        k,
+        l;
+    for (l = 0; l < 9; l += 1) {
+        for (k = 0; k < 9;) {
+            ar2 = array.slice();
+            shuffle(ar2);
+            strictEqual(ar2.length, 9);
+            // check all numbers are still elements of array
+            outer:
+                for (i = 0; i < 9; i += 1) {
+                    for (j = 0; j < 9; j += 1) {
+                        if (ar2[j] === i) {
+                            continue outer;
+                        }
+                    }
+                    ok(false);
+                }
+            // check every number comes at some time at position l
+            if (ar2[l] === k) {
+                k += 1;
+            }
+        }
+    }
+});
 
 test( "test candidateTrackField", function() {
     var ctField = createCandidateTrackField();
@@ -154,116 +269,3 @@ test( "test candidateTrackField getMinIndices", function() {
         cand: 1 << 3 // {3}
     }]);
 });
-
-test( "test internal util firstSetBit", function() {
-    var firstSetBit = hhelwi.sudoku.internals.firstSetBit;
-    strictEqual(firstSetBit(0), -1);
-    strictEqual(firstSetBit(1), 0);
-    strictEqual(firstSetBit(2), 1);
-    strictEqual(firstSetBit(3), 0);
-    strictEqual(firstSetBit(4), 2);
-    strictEqual(firstSetBit(~0), 0);
-    strictEqual(firstSetBit(1 << 31), 31);
-    // works only up to 32 bits as double64 gets converted to int32 for bit operations
-    strictEqual(firstSetBit(1 << 32), 0);
-});
-
-test( "test internal util numberOfSetBits", function() {
-    var numberOfSetBits = hhelwi.sudoku.internals.numberOfSetBits;
-    strictEqual(numberOfSetBits(0), 0);
-    strictEqual(numberOfSetBits(1), 1);
-    strictEqual(numberOfSetBits(2), 1);
-    strictEqual(numberOfSetBits(4), 1);
-    strictEqual(numberOfSetBits(8), 1);
-    strictEqual(numberOfSetBits(16), 1);
-    strictEqual(numberOfSetBits(3), 2);
-    strictEqual(numberOfSetBits(6), 2);
-    strictEqual(numberOfSetBits(15), 4);
-    strictEqual(numberOfSetBits(31), 5);
-    strictEqual(numberOfSetBits(7 << 29), 3);
-    strictEqual(numberOfSetBits(7 << 30), 2);
-    strictEqual(numberOfSetBits(7 << 31), 1);
-    strictEqual(numberOfSetBits(7 << 32), 3); // why?
-    strictEqual(numberOfSetBits(~0), 32);
-    strictEqual(numberOfSetBits(~0 << 1), 31);
-});
-
-test( "test solve", function() {
-    var field = hhelwi.sudoku.create(3, 3),
-        expected = [
-            5, 8, 2, 6, 7, 3, 4, 0, 1,
-            3, 7, 6, 4, 0, 1, 8, 2, 5,
-            0, 1, 4, 8, 5, 2, 7, 6, 3,
-            8, 2, 1, 5, 4, 0, 3, 7, 6,
-            4, 5, 7, 1, 3, 6, 2, 8, 0,
-            6, 3, 0, 2, 8, 7, 5, 1, 4,
-            2, 0, 8, 3, 6, 4, 1, 5, 7,
-            7, 4, 5, 0, 1, 8, 6, 3, 2,
-            1, 6, 3, 7, 2, 5, 0, 4, 8];
-    // minimal unique solvable field with 17 entries
-    field.set(0, 7, 0);
-    field.set(1, 0, 3);
-    field.set(2, 1, 1);
-    field.set(3, 4, 4);
-    field.set(3, 6, 3);
-    field.set(3, 8, 6);
-    field.set(4, 2, 7);
-    field.set(4, 6, 2);
-    field.set(5, 2, 0);
-    field.set(5, 4, 8);
-    field.set(6, 0, 2);
-    field.set(6, 3, 3);
-    field.set(6, 6, 1);
-    field.set(7, 1, 4);
-    field.set(7, 3, 0);
-    field.set(8, 3, 7);
-    field.set(8, 5, 5);
-    ok(field.solve());
-    deepEqual(field, expected, "validate solution");
-});
-
-test( "test solve empty", function() {
-    ok(hhelwi.sudoku.create(3, 3).solve(), "solve empty 9x9");
-});
-
-test( "test generate from empty", function() {
-    var field = hhelwi.sudoku.create(3, 3), f2;
-    ok(field.generate(), "generate from empty 9x9");
-    f2 = field.clone();
-    notDeepEqual(f2, field.solution);
-    f2.solve();
-    deepEqual(f2, field.solution);
-});
-
-test( "test shuffle", function() {
-    var shuffle = hhelwi.sudoku.internals.shuffle;
-    var array = [0,1,2,3,4,5,6,7,8],
-        ar2,
-        i,
-        j,
-        k,
-        l;
-    for (l = 0; l < 9; l += 1) {
-        for (k = 0; k < 9;) {
-            ar2 = array.slice();
-            shuffle(ar2);
-            strictEqual(ar2.length, 9);
-            // check all numbers are still elements of array
-            outer:
-            for (i = 0; i < 9; i += 1) {
-                for (j = 0; j < 9; j += 1) {
-                    if (ar2[j] === i) {
-                        continue outer;
-                    }
-                }
-                ok(false);
-            }
-            // check every number comes at some time at position l
-            if (ar2[l] === k) {
-                k += 1;
-            }
-        }
-    }
-});
-
-
